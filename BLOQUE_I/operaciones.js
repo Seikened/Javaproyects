4194c849d0c1b9f86076077e44f4d34182b7dcde
Seikened <fernanlee2131@gmail.com>
2021-10-18 22:14:03 -0500

    a

--- BLOQUE_I/operaciones.js
+++ BLOQUE_I/operaciones.js
@@ -1,6 +1,24 @@
-var a= 1;
-var b= 2;
+var a= 2;
+var b= 4;
 
 
-//suma
-var sum = a+b;
+var  suma = a+b;
+var resta = a-b;
+var multiplicacion = a*b;
+var division = a/b;
+var modulo_resto = a % b;
+var exponente = a**b;
+
+a++;
+
+a+=2;
+
+
+var numero = 2;
+var texto = 'Hola';
+var suma_combinada = numero + texto;
+
+console.log(suma_combinada);
+
+
+
