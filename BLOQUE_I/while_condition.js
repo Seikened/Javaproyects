e2e6f3cde9c0104f2516969e7d419e0bcedc0573
Seikened <fernanlee2131@gmail.com>
2021-10-23 23:46:39 -0500

    a

---
+++ BLOQUE_I/while_condition.js
@@ -0,0 +1,19 @@
+var contador =0;
+var N = 50;
+/*
+while (contador < N+1) {
+    console.log("Contador: " + contador);
+    contador++;
+}*/
+
+
+do {
+
+} while (contador)
+
+
+
+
+for (var i = 0; i < N; ++i) {
+    console.log("Contador: " + i);
+}
