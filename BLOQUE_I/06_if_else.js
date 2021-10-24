6f0c76237283086a99d1f561c711bdd6874cf4c3
Seikened <fernanlee2131@gmail.com>
2021-10-23 19:55:26 -0500

    a

--- BLOQUE_I/06_if_else.js
+++ BLOQUE_I/06_if_else.js
@@ -1,31 +1,34 @@
-//import all libraries necesaries to create a telegram Bot that can send messages to sms number  and create a telegram bot*/
-
-import TelegramBot from node-telegram-bot-api
-import request from 'request'
+// create a telegram bot with this token '2001341521:AAF9OlA1_ZykxIR_JgX__T6zy2G18CieFj8'
 
 
+// create a bot
+const TelegramBot = require('node-telegram-bot-api');
+const token = '2001341521:AAF9OlA1_ZykxIR_JgX__T6zy2G18CieFj8';
+const bot = new TelegramBot(token, {polling: true});
 
-//create a new bot that can send messages to sms number  and create a telegram bot
-const bot = new TelegramBot("2001341521:AAF9OlA1_ZykxIR_JgX__T6zy2G18CieFj8", {polling: true})
+
+// create a function to send a message to the user
+function sendMessage(chatId, message) {
+    bot.sendMessage(chatId, message);
+}
 
 
-//create a function that can send messages to sms number  and create a telegram bot
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id
-    const message = msg.text
-    const url = 'http://sms-gateway.me/api/mt/SendSMS?user=smsgateway&password=123456&senderid=senderid&channel=&DCS=0&flashsms=0&number=96655555555&text=message&route=1'
-    request(url, (error, response, body) => {
-        if(!error && response.statusCode == 200) {
-            bot.sendMessage(chatId, 'message sent')
-        } else {
-            bot.sendMessage(chatId, 'message not sent')
+// create a function to send a message to the user
+function sendMessageWithKeyboard(chatId, message, keyboard) {
+    bot.sendMessage(chatId, message, {
+        reply_markup: {
+            keyboard: keyboard,
+            resize_keyboard: true
         }
-    })})
+    });
+}
 
 
-
-
-
-
-
-
+// create a function to send a message to the user
+function sendMessageWithInlineKeyboard(chatId, message, inlineKeyboard) {
+    bot.sendMessage(chatId, message, {
+        reply_markup: {
+            inline_keyboard: inlineKeyboard
+        }
+    });
+}
