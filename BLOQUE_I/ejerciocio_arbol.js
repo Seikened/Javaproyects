736b4bbf3279ef890baec0b20f194e611cc7c9b9
Seikened <fernanlee2131@gmail.com>
2021-10-24 23:52:36 -0500

    a

--- BLOQUE_I/ejerciocio_arbol.js
+++ BLOQUE_I/ejerciocio_arbol.js
@@ -1,4 +1,4 @@
-var AlturaArbol = 10;
+/*var AlturaArbol = 10;
 
 for (p = 0; p < altura; p++) {
     console
@@ -8,4 +8,20 @@ for (p = 0; p < altura; p++) {
 }
 
 
+/*print "*" that a each interation of the loop increase by 1 in each side and the "*" need be centered
+when a change the hight of the tree this need be change in the loop*/
 
+var hight = 10;
+
+
+for (var i = 0; i < hight; i++) {
+    var star = "";
+    for (var j = 0; j < hight; j++) {
+        if (i == j || i + j == hight - 1) {
+            star += "*";
+        } else {
+            star += " ";
+        }
+    }
+    console.log(star);
+}
