b7e3ea94a9ee4ee76676d4e1cc9f762b5e9c70b3
Seikened <fernanlee2131@gmail.com>
2021-10-26 22:49:14 -0500

    a

---
+++ BLOQUE_I/09funciones.js
@@ -0,0 +1,14 @@
+// Formas de agrupar lógica
+function proceso1(){
+    console.log("Hago una cosa");
+    console.log("luego hago otra cosa");
+}
+
+
+
+// Las podemos llamar
+proceso1();
+// Repetidamente
+proceso1();
+
+
