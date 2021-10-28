ecb5922d116d5176e090b4f9362b1116a022d043
Seikened <fernanlee2131@gmail.com>
2021-10-28 00:07:12 -0500

    a

--- BLOQUE_III/index.js
+++ BLOQUE_III/index.js
@@ -1,3 +1,13 @@
 const readline = require('readline');
-const moment = require('moment');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  rl.question('Cuantas iteraciones quieres hacer?', function(iterations) {
+    for (let i = 0; i < iterations; ++i) {
+        console.log(i);
+    }
+    rl.close()
+  });
