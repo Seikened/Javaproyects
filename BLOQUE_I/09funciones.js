9e472de1438ceb9a90f60da7f7a694ff5b48ffec
Seikened <fernanlee2131@gmail.com>
2021-10-27 22:20:56 -0500

    s

--- BLOQUE_I/09funciones.js
+++ BLOQUE_I/09funciones.js
@@ -63,8 +63,37 @@ console.log("La media de " + firstNumber + " y " + secondNumber + " es " + midPo
 
 
 
+//▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ Esto es programación funcional ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
 
+// Las funciones son un tipo de dato, por lo que podemos utulizarlo como variable
+var logicaSaludar = function(nombre){ //esta función NO tiene nombre
+    console.log("Hola!, mi nombre es " + nombre); //En esta variable guardate toda esta función
+}
+
+// En logicaSaludar tenemos la referencia a la función
+// Podemos hacer algo tan loco como pasar una función a otra.
+function saludarSuperEducadamente(FuncionPasadaPorParametro, nombre){
+    console.log("Disculpe, perdone que le moleste, pero...");
+    // Ejecutamos la función que hemos recibidos
+    FuncionPasadaPorParametro(nombre); /*Aquí le mando el dato a la variable para que lo procese como función 
+    [logicaSaludar=saludaSuperEducadamente=FuncionPasadaPorParametro=FuncionPasadaPorParametro(nombre) =
+    = console.log("Hola!, mi nombre es " + nombre);]*/
+    console.log("Espero pasar una muy buena velada con usted");
+}
 
 
+//Si lo ejecutamos todo:
+saludarSuperEducadamente(logicaSaludar,"Fernando Leon");
 
+
+/*Segun yo que entiendo de este ejercicio, es que tenemos una
+ funcion que se convierte en una variable asi esta nos permite 
+ meterla como variable en "Otra función" asi llevando una función
+ dentro de otra, al final es un rollo muy interesante pero se llaman 
+ de una forma valida pasando de ser una función dentro de una variable, 
+ a ser una variable, despues una función que llama a la variable, se le 
+ asigna el valor y lo imprime en conjunto*/ 
+
+
+ 
